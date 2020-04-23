4b5a78a7b1c2d44c02d7b3e1f51408b6e833a1d3
Sanglier <a.sanglier67@gmail.com>
2020-04-23 12:47:59 +0100

    required

--- cliker.js
+++ cliker.js
@@ -2,6 +2,8 @@ var score = 0;
 var add = 1;
 var oven = 1;
 var addOven = 1;
+var money = 0;
+var addMoney = 1;
 
 function production(){
 
@@ -10,6 +12,10 @@ function production(){
 }
 function refresh(){
     score -= 25;
+    document.getElementById('pCanele').innerHTML = score;
+}
+function refreshMoney(){
+    score -= 100;
     document.getElementById('pCanele').innerHTML = score;
 }
 function buyOven(){
@@ -21,4 +27,21 @@ function buyOven(){
         document.getElementById('number').innerHTML = oven;
         refresh();
     }
+}
+function oseille(){
+    money += addMoney;
+}
+function sell(){
+    if (score >= 100){
+        oseille();
+        document.getElementById('monnaie').innerHTML = money;
+        refreshMoney();
+    }
+}
+function time(){
+    for (i=0; i<=100; i++) {
+        setTimeout(oseille(),60000);
+        document.getElementById('monnaie').innerHTML = money;
+    }
+    
 }
